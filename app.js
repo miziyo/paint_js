91cb008bbfafe60b7906ab71d677989873b5d62b
copley <74097686+miziyo@users.noreply.github.com>
2021-07-22 19:44:53 +0900

    add button and can create  line title is contents

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokestyle = "#000000";
+ctx.lineWidth = 2.5;
 
 let painting = false;
+
+function stopPaining() {
+	painting = false;
+}
+
+function startPainting() {
+	painting = true;
+}
 
 function onMouseMove(event) {
 	const x = event.offsetX;
 	const y = event.offsetY;
-}
-
-function onMouseDown(event) {
-	painting = true;
-}
-
-function onMouseUp(event) {
-	painting = false;
-}
-
-function onMouseLeave(event) {
-	stopPaining();
+	if (!painting) {
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+	} else {
+		ctx.lineTo(x, y);
+		ctx.stroke();
+	}
 }
 
 if (canvas) {
 	canvas.addEventListener("mousemove", onMouseMove);
-	canvas.addEventListener("mousedown", onMouseDown);
-	canvas.addEventListener("mouseup", onMouseUp);
+	canvas.addEventListener("mousedown", startPainting);
+	canvas.addEventListener("mouseup", stopPaining);
 	canvas.addEventListener("mouseleave", stopPaining);
 }
